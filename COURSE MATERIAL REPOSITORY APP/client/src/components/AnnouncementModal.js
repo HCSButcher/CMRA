1fafc916e8a3c93632b767e208698442862d4c18
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-07-13 13:31:44 +0300

    Update AnnouncementModal.js

--- COURSE MATERIAL REPOSITORY APP/client/src/components/AnnouncementModal.js
+++ COURSE MATERIAL REPOSITORY APP/client/src/components/AnnouncementModal.js
@@ -19,7 +19,7 @@ const AnnouncementModal = () => {
 
         try {
             const response = await axios.post(
-                "http://10.1.33.99:3001/announcements",
+                "https://project-2-1u71.onrender.com/announcements",
                 { unit, email, date, announcements },
                 { headers: { Authorization: `Bearer ${token}` } }
             );
